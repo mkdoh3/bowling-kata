33fabf374c8bca7c27e7644817393caa10f7a155
mkdoh <mkdoherty3@gmail.com>
2022-06-30 19:03:02 -0500

    refactor(lib) refactors game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,36 +1,60 @@
 class Game
-    def score(frames)
-        score = 0
-        frames.each_with_index do |rolls, frame|
-            score += rolls[0]
-            next_frame = frames[frame + 1]
-            if(rolls[0] === 10) #is it a strike?
-                if(frame === 9) #is it the 10th frame?
-                    score += rolls[1] + rolls[2]
-                elsif(frame === 8) #is it the 9th frame?
-                    score += next_frame[0]
-                    score += next_frame[1]
-                elsif(next_frame[0] === 10) #is the next roll also a strike?
-                    score += next_frame[0]
-                    score += frames[frame + 2][0]
-                else
-                    score += next_frame[0]
-                    score += next_frame[1]
-                end
-            elsif(rolls[0] + rolls[1] === 10) #is it a spare?
-                if(frame === 9) #is it a spare in the 10th frame?
-                    score += rolls[1]
-                    score += rolls[2]
-                else
-                    score += rolls[1]
-                    score += next_frame[0]
-                end
-            else
-                score += rolls[1]
-            end
+    attr_accessor :score, :frames, :turn
+
+    def initialize
+      @score = 0
+      @turn = 0
+      @frames = []
+    end
+
+    def tenth_frame?
+        turn === 9
+    end
+
+    def ninth_frame?
+        turn === 8
+    end
+
+    def normal_frame?
+        turn < 8
+    end
+
+    def strike?
+        roll(1) === 10
+    end
+
+    def spare?
+        roll(1) + roll(2) === 10
+    end
+
+    def total_of_next_two_rolls
+        return roll(2) + roll(3) if tenth_frame?
+        return roll(1,1) +  roll(1,2) if roll(1,1) === 10 && normal_frame?
+        roll(1,1) + roll(2,1)
+    end
+
+    def add_strike
+        @score += total_of_next_two_rolls
+    end
+
+    def add_spare
+        @score += roll(2)
+        return @score += roll(3) if tenth_frame?
+        return @score += roll(1,1)
+    end
+
+    def roll(ball, step = 0)
+        frames[turn + step][ball - 1]
+    end
+
+    def calculate_score
+        frames.each_index do |index|
+            @turn = index
+            @score += roll(1)
+            next add_strike if strike?
+            next add_spare if spare?
+            @score += roll(2)
         end
-        score
     end
 end
 
-frames = [[1,9],[10],[10],[10],[6,4],[10],[10],[10],[10],[10,10,10]]
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,26 +5,30 @@ RSpec.describe Game do
     describe '#score' do
         context "with no strikes or spares" do
             it 'should return the correct score for the given frames' do
-            frames = [[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]]
-            expect(game.score(frames)).to eq(20)
+            game.frames = [[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]]
+            game.calculate_score
+            expect(game.score).to eq(20)
             end
         end
         context "with strikes and spares throughout" do
             it 'should return the correct score for the given frames' do
-            frames = [[1,9],[10],[5,3],[7,2],[6,4],[9,0],[10],[10],[4,3],[5,4]]
-            expect(game.score(frames)).to eq(140)
+            game.frames = [[1,9],[10],[5,3],[7,2],[6,4],[9,0],[10],[10],[4,3],[5,4]]
+            game.calculate_score
+            expect(game.score).to eq(140)
             end
         end
         context "with a spare in the 10th frame" do
             it 'should return the correct score for the given frames' do
-            frames = [[1,9],[10],[10],[10],[6,4],[10],[10],[10],[10],[2,8,10]]
-            expect(game.score(frames)).to eq(238)
+            game.frames = [[1,9],[10],[10],[10],[6,4],[10],[10],[10],[10],[2,8,10]]
+            game.calculate_score
+            expect(game.score).to eq(238)
             end
         end
          context "with a strike in every frame" do
             it 'should return the correct score for the given frames' do
-            frames = [[10],[10],[10],[10],[10],[10],[10],[10],[10],[10,10,10]]
-            expect(game.score(frames)).to eq(300)
+            game.frames = [[10],[10],[10],[10],[10],[10],[10],[10],[10],[10,10,10]]
+            game.calculate_score
+            expect(game.score).to eq(300)
             end
         end
     end
