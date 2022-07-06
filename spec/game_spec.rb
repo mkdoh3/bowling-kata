59ee4962a47bfbb8cb77e7c8546a3333f96f9a05
mkdoh <mkdoherty3@gmail.com>
2022-07-06 10:21:19 -0500

    refactor(spec): adds context to unit tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,62 +4,78 @@ RSpec.describe Game do
     let(:game) { Game.new }
 
     describe '#ninth_frame?' do
-        it 'returns true if turn is 8' do
-            game.turn = 8
-            expect(game.ninth_frame?).to be true
+        context 'when it is the 8th turn' do
+            it 'returns true' do
+                game.turn = 8
+                expect(game.ninth_frame?).to be true
+            end
         end
 
-        it 'returns false if turn is not 8' do
-            game.turn = 1
-            expect(game.ninth_frame?).to be false
+        context 'when it is not the 8th turn' do
+            it 'returns false' do
+                game.turn = 1
+                expect(game.ninth_frame?).to be false
+            end
         end
     end
 
     describe '#tenth_frame?' do
-        it 'returns true if turn is 9' do
-            game.turn = 9
-            expect(game.tenth_frame?).to be true
+        context 'when it is the 9th turn' do
+            it 'returns true' do
+                game.turn = 9
+                expect(game.tenth_frame?).to be true
+            end
         end
-
-        it 'returns false if turn is not 9' do
-            game.turn = 1
-            expect(game.tenth_frame?).to be false
+        context 'when it is not the 9th turn' do
+            it 'returns false' do
+                game.turn = 1
+                expect(game.tenth_frame?).to be false
+            end
         end
     end
 
     describe '#normal_frame?' do
-        it 'returns true if turn is less than 8' do
-            game.turn = 7
-            expect(game.normal_frame?).to be true
+        context 'when the turn number is less than 8' do
+            it 'returns true' do
+                game.turn = 7
+                expect(game.normal_frame?).to be true
+            end
         end
-
-        it 'returns false if turn is 8 or greater' do
-            game.turn = 8
-            expect(game.normal_frame?).to be false
+        context 'when the turn number is equal to or greater than 8' do
+            it 'returns false' do
+                game.turn = 9
+                expect(game.normal_frame?).to be false
+            end
         end
     end
 
     describe '#strike?' do
-        it 'returns true if the first roll of the frame is 10' do
-            game.frames = [[10]]
-            expect(game.strike?).to be true
+        context 'when the first roll of a frame is 10' do
+            it 'returns true' do
+                game.frames = [[10]]
+                expect(game.strike?).to be true
+            end
         end
-
-          it 'returns false if the first roll of the frame is not 10' do
-            game.frames = [[1]]
-            expect(game.strike?).to be false
+        context 'when the first roll of a frame is not 10' do
+            it 'returns false' do
+                game.frames = [[1]]
+                expect(game.strike?).to be false
+            end
         end
     end
 
     describe '#spare?' do
-         it 'returns true if both rolls of the frame equal 10' do
-            game.frames = [[1,9]]
-            expect(game.spare?).to be true
+        context 'when the sum of the first two rolls of a frame is 10' do
+            it 'returns true' do
+                game.frames = [[1,9]]
+                expect(game.spare?).to be true
+            end
         end
-
-          it 'returns false if both rolls of the frame do not equal 10' do
-            game.frames = [[1, 1]]
-            expect(game.spare?).to be false
+        context 'when the sum of the first two rolls of a frame is not 10' do
+            it 'returns false' do
+                game.frames = [[1, 1]]
+                expect(game.spare?).to be false
+            end
         end
     end
 
@@ -134,33 +150,33 @@ RSpec.describe Game do
     describe '#calculate_score' do
         context "with no strikes or spares" do
             it 'should return the correct score for the given frames' do
-            game.frames = [[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]]
-            game.calculate_score
-            expect(game.score).to eq(20)
+                game.frames = [[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]]
+                game.calculate_score
+                expect(game.score).to eq(20)
             end
         end
 
         context "with strikes and spares throughout" do
             it 'should return the correct score for the given frames' do
-            game.frames = [[1,9],[10],[5,3],[7,2],[6,4],[9,0],[10],[10],[4,3],[5,4]]
-            game.calculate_score
-            expect(game.score).to eq(140)
+                game.frames = [[1,9],[10],[5,3],[7,2],[6,4],[9,0],[10],[10],[4,3],[5,4]]
+                game.calculate_score
+                expect(game.score).to eq(140)
             end
         end
 
         context "with a spare in the 10th frame" do
             it 'should return the correct score for the given frames' do
-            game.frames = [[1,9],[10],[10],[10],[6,4],[10],[10],[10],[10],[2,8,10]]
-            game.calculate_score
-            expect(game.score).to eq(238)
+                game.frames = [[1,9],[10],[10],[10],[6,4],[10],[10],[10],[10],[2,8,10]]
+                game.calculate_score
+                expect(game.score).to eq(238)
             end
         end
 
          context "with a strike in every frame" do
             it 'should return the correct score for the given frames' do
-            game.frames = [[10],[10],[10],[10],[10],[10],[10],[10],[10],[10,10,10]]
-            game.calculate_score
-            expect(game.score).to eq(300)
+                game.frames = [[10],[10],[10],[10],[10],[10],[10],[10],[10],[10,10,10]]
+                game.calculate_score
+                expect(game.score).to eq(300)
             end
         end
     end
