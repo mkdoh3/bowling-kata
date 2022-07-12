8b59668560d6aea942343abfcc25ccaa344fb0e7
mkdoh <mkdoherty3@gmail.com>
2022-07-12 13:00:42 -0500

    refactor(lib) refactors calculator class. adds spare value and strike value methods

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,19 +1,23 @@
 class Calculator
 
+    def self.strike_values(current_frame, next_frame, third_frame, turn)
+        return current_frame[1] + current_frame[2] if turn === 9
+        return next_frame[0].to_i + third_frame[0].to_i if next_frame[0].to_i === 10 && turn < 8
+        next_frame[0].to_i + next_frame[1].to_i
+    end
+
+    def self.spare_value(current_frame, next_frame, turn)
+        return current_frame[2] if turn === 9
+        next_frame[0]
+    end
+
+
     def self.add_values(current_frame, next_frame, third_frame, turn)
-        total = current_frame[0]
-        if(total === 10)
-            return total += current_frame[1] + current_frame[2] if turn === 9
-            total += next_frame[0].to_i
-            return total += third_frame[0].to_i if total === 20 && turn < 8
-            return total += next_frame[1].to_i
-        end
-        total += current_frame[1]
-        if(total === 10)
-            return total += current_frame[2] if turn === 9
-            return total += next_frame[0]
-        end
-        total
+        frame_total = current_frame[0]
+        return frame_total += strike_values(current_frame, next_frame, third_frame, turn) if frame_total === 10
+        frame_total += current_frame[1]
+        return frame_total += spare_value(current_frame, next_frame, turn) if frame_total === 10
+        frame_total
     end
 
     def self.score(frames)
